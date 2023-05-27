80449468698953f9e29142cd9f40c7e273181386
athirabmenon02 <84238693+athirabmenon02@users.noreply.github.com>
2023-05-27 21:41:17 +0530

    Delete html directory

--- html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: homepage.html"); // Replace "index.html" with the actual URL of your HTML home page
-exit();
-?>
